51cb50f95038b521bf95e7e5a25b4f130c3ae97f
navgurukul <ranisingh21@navgurukul.org>
2023-11-30 00:29:51 +0530

    working on body

--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.css";
 import Header from './Header'
 import './globals.css'
-import SliderBox from './main.js'
+
 
 const page = () => {
   return (
     <>
       <Header/>
-       <br></br>
-      <SliderBox/>
+
+     
     </>
   )
 }
