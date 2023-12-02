994e646fb1c8a1d7264d2abd0706df52e5e1c9a5
navgurukul <ranisingh21@navgurukul.org>
2023-12-02 19:20:37 +0530

    error

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './Header';
 import './globals.css';
 import Nav from './nav.js';
+import Slider from './slider.js';
 
 const Page = () => {
   return (
     <>
       <Nav />
       <Header />
+      <Slider/>
    
     </>
   );
