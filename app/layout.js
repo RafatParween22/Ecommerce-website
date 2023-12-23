d0e38ce092a0919726929c32527ae8bc3cb94b7c
rukhsana <rukhsana21@navgurukul.org>
2023-12-23 15:26:08 +0530

    detaile page is complete

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from 'next/font/google'
+import Nav from './nav.js';
 import './globals.css'
+import Footer from './footer.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}> 
+      <div><Nav/></div>
+      {children}</body>
+      <div><Footer/></div>
     </html>
   )
 }
--- app/listing/page.js
+++ app/listing/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Listing() {
   return (
-    <div className="listing">Product-Listing</div>
+    <div className="listing">product listing in that page</div>
   )
 }
 
