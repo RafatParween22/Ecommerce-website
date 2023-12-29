cc09d9e5884b3a15eab8ca9f6ea42cf8af56177e
rukhsana <rukhsana21@navgurukul.org>
2023-12-29 15:11:28 +0530

    some changes

--- app/products/[category]/page.js
+++ app/products/[category]/page.js
@@ -6,6 +6,11 @@ import Link from 'next/link';
 function Page({ params }) {
   const [productDetails, setProductDetails] = useState([]);
 
+  const [searchValue, setSearchValue] = useState('');
+  const [filterValue, setFilterValue] = useState(0);
+  const [minFilter, setMinFilter] = useState();
+  const [maxFilter, setMaxFilter] = useState();
+
   const fetchProductDetails = () => {
     fetch(`https://fakestoreapi.com/products/category/${params.category}`)
       .then((res) => res.json())
@@ -26,26 +31,93 @@ function Page({ params }) {
   }
 
   return (
-    <div>
-      <p>Product List-{params.category}</p>
-        <div className="container">
-            <div className="row">
-                {productDetails.map((product) => (
-                    <div key={product.id} className="col-3 ">
-                    <div className="card">
-                    <div className="card-body">
-                        <img src={product.image} alt={product.title} className="img-fluid" />
-                        <div className="card-body text-center">
-                        <h5 className="card-title" style={{ fontWeight: '500' }}>
-                            {product.title}
-                        </h5>
-                        </div>
-                    </div>
-                    </div>
-                </div>))}
-            </div>    
-      </div> 
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-3 ">
+          <div className="row mx-2"> ******** filter********</div>
+
+            <div class="priceFilter">
+                <p> Price:</p>
+                <div>
+                <input
+                    className="minInput"
+                    label="Price"
+                    placeholder="Min..."
+                    value={minFilter}
+                    onChange={(e) => {
+                    setMinFilter(e.target.value);
+                    }}
+                />
+                </div>
+
+                <div>
+                <input
+                    className="minInput"
+                    placeholder="Max..."
+                    value={maxFilter}
+                    onChange={(e) => {
+                    setMaxFilter(e.target.value);
+                    }}
+                />
+                </div>
+
+                <button onClick={() => setFilterValue(1)}>Apply</button>
+            </div>
+        </div>
+
+
+        <div className="col-sm">
+          <div className="card ">
+            <div className=" col-2">
+              {productDetails.filter((x) => {
+                  if (searchValue == '') return x;
+                })
+                .filter((cost) => {
+                  if (filterValue == 0) return cost;
+                  else if (cost.price >= minFilter && cost.price <= maxFilter) {
+                    return cost;
+                  }
+                })
+                .map((items) => (
+                  <div key={items.id} className="col-sm-4 grid-img">
+                      <div className="card1">
+                          <div className=" imageDesc ">
+                            <p className="descLine">{items.title}</p>
+                          <div>
+                          <img
+                            src={items.image}
+                            style={{ width: '300px', height: '300px', borderRadius: '8px' }} 
+                          />
+                      </div>
+
+                      <div className="productDesc">
+                        <p className="price">
+                          ₹{items.price}
+                          <span className="priceCut">
+                            ₹{items.priceCut}
+                          </span>
+                          <span className="offer">{items.offer}%0ff</span>
+                        </p>
+                      </div>
+
+
+                      <div className="col">
+                        <p>{items.descreption}</p>
+                      </div>
+
+
+                  </div>
+
+                </div>
+                  
+                </div>
+                ))}
+            </div>
+          </div>
+        </div>
     </div>
+
+    //  </div>
   );
 }
 
